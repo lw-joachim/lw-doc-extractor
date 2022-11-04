1d2d6775b48c32b4e37fe12fd60e2fa559da2aa3
lw-joachim <kestner@lightword.de>
2022-11-04 18:00:52 +0100

    updated run utility

--- run.py
+++ run.py
@@ -21,18 +21,12 @@ def prog4():
         al = json.load(fh)
     tools.generate_audio_files(al, "audio_out", "test_files\\tts_key.json")
     
-
 def prog2():
     logging.basicConfig(level=logging.DEBUG)
     with open(r"C:\git\lw-doc-extractor\test_files\debug\lexer_output.json") as fh:
         ast = json.load(fh)
         story_compiler.compile_story(ast)
-        
-def prog3():
-    if "shoebill" == socket.gethostname():
-        old_sys_argv = sys.argv
-        sys.argv = [old_sys_argv[0]] + ["-vv", "test_files\comp_output.json", "--auth_file", "test_files\mycred", "--project", "OneArticy"]
-    cli.run_populator_main()
+
     
 def prog31():
     if "shoebill" == socket.gethostname():
@@ -50,6 +44,40 @@ def prog1():
         sys.argv = [old_sys_argv[0]] + ["-v", "test_files\Levi's Feast - ArticyScript.docx", "-o", "test_files\comp_output.json"]
     cli.main()
     
+
+def prog1r():
+    print(socket.gethostname())
+    if "shoebill" == socket.gethostname():
+        old_sys_argv = sys.argv
+        sys.argv = [old_sys_argv[0]] + ["-v", "C:\work\plastic_cloud\ONEof500-Game\One\Content\Story\LF\LF.docx", "-o", "C:\work\plastic_cloud\ONEof500-Game\One\Content\Story\LF\compiler_output.json"]
+    cli.main()
+
+def prog3r():
+    if "shoebill" == socket.gethostname():
+        old_sys_argv = sys.argv
+        sys.argv = [old_sys_argv[0]] + ["-v", "C:\work\plastic_cloud\ONEof500-Game\One\Content\Story\LF\compiler_output.json", "--auth_file", "test_files\mycred", "--project", "OneArticy"]
+    cli.run_populator_main()
+    
+def prog3t():
+    if "shoebill" == socket.gethostname():
+        old_sys_argv = sys.argv
+        sys.argv = [old_sys_argv[0]] + ["-v", "C:\work\plastic_cloud\ONEof500-Game\One\Content\Story\LF\compiler_output.json", "--auth_file", "test_files\mycred"]
+    cli.run_populator_main()
+    
+def prog5r():
+    logging.basicConfig(level=logging.DEBUG)
+    if "shoebill" == socket.gethostname():
+        old_sys_argv = sys.argv
+        sys.argv = [old_sys_argv[0]] + ["-v", "C:\work\plastic_cloud\ONEof500-Game\One\Content\Story\LF\compiler_output.json", "test_files\\real\\lines.json"]
+        
+    tools.extract_dialog_lines()
+    
+    with open("test_files\\real\\lines.json") as fh:
+        al = json.load(fh)
+    tools.generate_audio_files(al, "C:\work\plastic_cloud\ONEof500-Game\One\Content\Story\LF\\audio_out", "test_files\\tts_key.json")
+    
+    
 if __name__ == '__main__':
-    prog1()
-    prog3()
+    #prog1r()
+    #prog3r()
+    prog3r()
